7e9c28500808da2542d513b34894bb7805746507
Martin Algañaraz <idcmardelplata@gmail.com>
2017-09-22 14:16:53 -0300

    Agregando soporte para servir archivos estaticos metiante inert

---
+++ public/js/file.js
@@ -0,0 +1,1 @@
+console.log("hey buddy, there you go!")
--- server.js
+++ server.js
@@ -9,9 +9,33 @@ server.connection({
 })
 
 
-server.register([
+server.register({
+  register: require('inert')
+}, (err) => {
+  if  (err) throw err
 
-])
+  server.route({
+    method: 'GET',
+    path: '/file.js',
+    handler: (request, reply) => {
+      reply.file('public/js/file.js')
+    } 
+  })
+
+  server.route({
+    method: 'GET',
+    path: '/js/{file*}',
+    handler: {
+      directory: {
+        path: 'public/js',
+        listing: false
+      }
+    }
+  })
+
+})
+
+
 
 server.start(function(err) {
   if (err) throw err
