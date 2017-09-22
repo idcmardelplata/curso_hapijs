b6a37ab1814533cf26aed75bd1a48229dd7e1c8d
Martin Algañaraz <idcmardelplata@gmail.com>
2017-09-22 20:27:27 -0300

    Respondiendo en formato json

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 'use strict'
 
 const Hapi = require('hapi')
-const Boom = require('boom')
 
 const server = new Hapi.Server()
 
@@ -10,60 +9,25 @@ server.connection({
   port: 3000
 })
 
-
-const rootHandler = (request, reply) => {
-  reply.view('index', {
-    title: 'examples/views/jade/index.js | Hapi',
-    message: 'Index - Hello World'
-  })
-}
-
-const aboutHandler = (request, reply) => {
-  reply.view('about', {
-    title: 'examples/views/jade/index.js | Hapi',
-    message: 'About - Hello World'
-  })
-}
-
-server.register(require('vision'), (err) => {
-  if (err) throw err
-  
-  server.views({
-    engines: {
-      pug: require('pug')
-    },
-    path: './views',
-    compileOptions: {
-      pretty: true
-    }
-  })
-
-  server.route({method: 'GET', path: '/', handler: rootHandler})
-  server.route({method: 'GET', path: '/about', handler: aboutHandler})
-  server.route({
-    method: 'GET',
-    path: '/code',
-    config: {
-      response: {
-        emptyStatusCode: 204
-      },
-      handler: (request, reply) => reply() 
-    }
-  })
-  server.route({
-    method: 'GET',
-    path:'/error',
-    config: {
-      handler: (req, res) => {
-        res(Boom.badRequest('Your data is invalid'))
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: (req, res) => {
+    const data = {
+      id: 123,
+      name: 'Martin Algañaraz',
+      isDeveloper: true,
+      func: function() {
+        return 1
       }
     }
-  })
+    res(data).code(201)
+  }
+})
 
-  server.start(function(err) {
-    if (err) throw err
-    console.log(`Server start at ${server.info.uri}`)
-  })
+server.start((err) => {
+  if (err) console.error(`Error al inicializar el servidor: ${err}`)
+  console.log(`Server corriendo en ${server.info.uri}`)
 })
 
 
